719603669dc893e77d9c0e743e3879de6a0091ab
murilloslopes <murillosivierolopes2310@gmail.com>
2026-05-07 23:09:45 -0300

    feat: aula 07 subindo repositorios

---
+++ js/script.js
@@ -0,0 +1,32 @@
+/*
+Declaracao com constante 
+
+
+// Conversões:
+let numero = "123";
+console.log(typeof numero); // string
+console.log(parseInt(numero)); // 123
+console.log(typeof parseInt(numero)); // number
+console.log(parseFloat("12.5")); // 12.5
+console.log(typeof parseFloat("12.5")); // number
+console.log(numero.toString()); // “123”
+// Métodos de texto:
+let frase = "Estão chegando as provas!";
+console.log(frase.length); // 26 – Retorna a quantidade de caracteres da string
+ 
+console.log(frase.indexOf("as")); // 15 – Retorna a posição da string encontrada
+ 
+console.log(frase.substring(frase.indexOf("ch"), (frase.length))); // “as provas”
+ 
+console.log(frase.substring(15, 25)); // “as provas”
+ 
+console.log(frase.replace("provas", "férias")); // “Estão chegando as férias!”
+ 
+// Formatação de número:
+let valor = 123.456;
+console.log(valor.toFixed(2)); // “123.46”
+console.log(valor.toPrecision(4)); // “123.5”
+
+
+ */
+
