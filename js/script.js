a58f2d120757bc2df0a7b29b51b8897b7ab5a443
adaltonagabe <adalto.nagabe@gmail.com>
2026-05-14 20:01:42 -0300

    feat : Erros nos html e javascript corrigidos

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 //Criando uma lista de objetos:
 const usuarios = [
-    {id: 1, email: "email@email.com", senha: "12345", nome:"Jose das Couves", avatar="https://placehold.co/100x100/d1d5db/111827.png?text=Mobile%20Preview&font=inter"},
-    {id: 2, email: "nadir@email.com", senha: "12345", nome:"Nadir das Batatas", avatar="https://placehold.co/100x100/d1d5db/111827.png?text=Mobile%20Preview&font=inter"},
-    {id: 3, email: "joel@email.com", senha: "12345", nome:"Joel da Candida", avatar="https://placehold.co/100x100/d1d5db/111827.png?text=Mobile%20Preview&font=inter"},
-    {id: 4, email: "tonho@email.com", senha: "12345", nome:"Antonio das Alfaces", avatar="https://placehold.co/100x100/d1d5db/111827.png?text=Mobile%20Preview&font=inter"},
-    {id: 5, email: "jo@email.com", senha: "12345", nome:"Joaquim das Jacas", avatar="https://placehold.co/100x100/d1d5db/111827.png?text=Mobile%20Preview&font=inter"},
+    {id: 1, email:"email@email.com",senha:"12345",nome:"José das Couves",avatar : "https://placehold.co/100x100/d1d5db/111827.png?text=Mobile%20Preview&font=inter"},
+    {id: 2, email:"nadir@email.com",senha:"12345",nome:"Nadir das Batatas",avatar : "https://placehold.co/100x100/d1d5db/111827.png?text=Mobile%20Preview&font=inter"},
+    {id: 3, email:"joel@email.com",senha:"12345",nome:"Joel da Cândida",avatar : "https://placehold.co/100x100/d1d5db/111827.png?text=Mobile%20Preview&font=inter"},
+    {id: 4, email:"tonho@email.com",senha:"12345",nome:"Antonio das Alfaces",avatar : "https://placehold.co/100x100/d1d5db/111827.png?text=Mobile%20Preview&font=inter"},
+    {id: 5, email:"jo@email.com",senha:"12345",nome:"Jaquim das Jacas",avatar : "https://placehold.co/100x100/d1d5db/111827.png?text=Mobile%20Preview&font=inter"},
 ];
 
-//Compare os dados do form contra a lista de usuarios
-//Utilize estrutura de repeticao
-//Imprima a lista de usuarios
+//Compare os dado do form contra a lista de usuários
+//Utilize estruura de repetição
+//Imprima a lista de usuários.
 
 
 
@@ -18,69 +18,64 @@ const usuarios = [
 const botao = document.getElementById("btnEntrar");
 
 botao.addEventListener("click", function(e){
-
     e.preventDefault();
     //Capturando dados do campo de email e senha
-    const email = document.getElementById("IdEmail");
-    const senha = document.getElementById("IdSenha");
-
-    //Declarando um ojeto:    
+    const email = document.getElementById("idEmail");
+    const senha = document.getElementById("idSenha");
+    
+    //Declarando um objeto:
     let usuario = {
-    email: email.value,
-    senha: senha.value
+        email: email.value,
+        senha: senha.value
     }
 
     console.log( usuario.email );
     console.log( usuario.senha );
 
+
     const userDoBanco = {
-        email : "email@email.com",
-        senha : "12345",
-        nome : "Jose das Couves",
+        email  : "email@email.com",
+        senha  : "12345",
+        nome   : "José das Couves",
         avatar : "https://placehold.co/100x100/d1d5db/111827.png?text=Mobile%20Preview&font=inter"
     }
 
-    //Validacao de dados do usuario
+    //Validação de dados do usuário
 
     try {
         if(usuario !== null){
-            if((usuario.email === userDoBanco.email) && (usuario.senha === userDoBanco.senha)){
-                alert("Login realizado com sucesso!");
+            if( (usuario.email === userDoBanco.email) && (usuario.senha === userDoBanco.senha) ){
+                    alert("Login realizado com sucesso!");
 
-                //Informando o usuario que ele sera direcionado!
-                const divMsg = document.getElementById("msg");
+                    //Informando o usuário que ele será redirecionado!
+                    const divMsg = document.getElementById("msg");
 
-                //Adicionando a mensagem em um <p></p> atraves de innerHTML
-                divMsg.innerHTML = "<p>Voce sera redirecionado em 5 segundos...</p>"
+                    //Adicionando a msg em um <p></p> através de innerHTML
+                    divMsg.innerHTML = "<p>Você será redirecionado em 5 segundo.....</p>"
 
-                setTimeout( function(){
-                    //Refirecionando o usuario para a pagina inicial!
-                    window.location.href = "../index.html";
-                },5000);
-
-
+                    setTimeout(function(){
+                        //Redirecionando o usuário para a página de inicial!
+                        window.location.href = "../index.html"; 
+                    },5000);
             }else{
-               throw new Error("Email ou senha incorretos!")
+                throw new Error("Email ou senha incorretos!");
             }
         }else{
-            throw new Error("Usuario nao encontrado")    
+            throw new Error("Usuário não encontrado!");
         }
-    
-    } catch (error){
+        
+    } catch (error) {
         console.error(error);
         alert(error);
     }
-    
-
-    
-    
 
 
 
-    //Crie um novo objeto com as propriedades email e senha
-    //Compare contra os dados do usuario
-    //Crie uma validacao para os campos de email e senha
-    //Se a validacao for bem sucedida exibir mensagem de sucesso caso contrario exiba a mensagem de erro
+    //Crie um novo objeto com as propriedades email e senha.
+    //Compare contra os dados do usuário.
+    //Crie uma validação para os campos de e-mail e senha.
+    //Se a validacão for bem sucedida, exibir mensagem de sucesso, caso contrário, exiba mensagem de erro.
 
 
-});
+
+}); 
