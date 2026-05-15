7179310bfcedcd48c75d969ae612b7fd10140b03
adaltonagabe <adalto.nagabe@gmail.com>
2026-05-14 23:15:30 -0300

    feat : Estruturas de repeticao javascript

--- js/script.js
+++ js/script.js
@@ -7,18 +7,39 @@ const usuarios = [
     {id: 5, email:"jo@email.com",senha:"12345",nome:"Jaquim das Jacas",avatar : "https://placehold.co/100x100/d1d5db/111827.png?text=Mobile%20Preview&font=inter"},
 ];
 
-//Compare os dado do form contra a lista de usuários
-//Utilize estruura de repetição
+//o que e uma string? podemos dizer que e um array de caracteres
+/* let frase = "Estou com muito sono!";
+/* console.log(frase[6]); */
+/* console.log(frase.length); */
+
+/* for(let x = 0; x < frase.length; x++){
+    if(frase[x].toLowerCase() === "o")
+        console.log(x,frase[x]);
+    
+} */ 
+
+//Percorrendo a lista de usuários utilizando estrutura de repetição:
+for (let i = 0; i < usuarios.length; i++){
+    console.log(usuarios[i].id,usuarios[i].nome);
+}
+
+
+//Compare os dados do form contra a lista de usuários
+//Utilize estrutura de repetição
 //Imprima a lista de usuários.
 
 
 
 
 
-const botao = document.getElementById("btnEntrar");
+//const botao = document.getElementById("btnEntrar"); tambem ta certo
+const botao = document.querySelector("#btnEntrar");
 
 botao.addEventListener("click", function(e){
+    
+    console.log(this.textContent); //Quando utilizamos o this, estamos realizando uma referência ao elemento principal.
     e.preventDefault();
+
     //Capturando dados do campo de email e senha
     const email = document.getElementById("idEmail");
     const senha = document.getElementById("idSenha");
@@ -79,3 +100,27 @@ botao.addEventListener("click", function(e){
 
 
 }); 
+
+
+/* function validar(){
+    //funcao tradicional
+    console.log("NAO EXECUTE!!");
+} */
+
+/* function validar(a,b){
+    let resultado = a + b;
+    return resultado;
+}
+
+console.log(validar(2,2)); */
+
+/* const validar = (a,b) => a+b;
+console.log(validar(2,2)); */
+
+/* const validar = (a,b) =>{
+    return a+b;
+}
+console.log(validar(2,2)); */
+
+
+    //Tente realizar a validacao da lista contra o objeto que recupera os dados fora do form.
