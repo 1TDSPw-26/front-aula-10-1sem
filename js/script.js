aabbda3292637bfaabcce7a4f155de636c0180ae
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2026-05-19 20:52:44 -0300

    feat : Corrigindo redirect com setInterval e utilização de lista na validação do login.

--- js/script.js
+++ js/script.js
@@ -1,28 +1,11 @@
 //Criando uma lista de objetos:
 const usuarios = [
-    {id: 1, email:"email@email.com",senha:"12345",nome:"José das Couves",avatar : "https://placehold.co/100x100/d1d5db/111827.png?text=Mobile%20Preview&font=inter"},
-    {id: 2, email:"nadir@email.com",senha:"12345",nome:"Nadir das Batatas",avatar : "https://placehold.co/100x100/d1d5db/111827.png?text=Mobile%20Preview&font=inter"},
-    {id: 3, email:"joel@email.com",senha:"12345",nome:"Joel da Cândida",avatar : "https://placehold.co/100x100/d1d5db/111827.png?text=Mobile%20Preview&font=inter"},
-    {id: 4, email:"tonho@email.com",senha:"12345",nome:"Antonio das Alfaces",avatar : "https://placehold.co/100x100/d1d5db/111827.png?text=Mobile%20Preview&font=inter"},
-    {id: 5, email:"jo@email.com",senha:"12345",nome:"Jaquim das Jacas",avatar : "https://placehold.co/100x100/d1d5db/111827.png?text=Mobile%20Preview&font=inter"},
+    {id: 1, email:"email@email.com",senha:"12345",nome:"José Menino",avatar : "https://placehold.co/100x100/d1d5db/111827.png?text=Mobile%20Preview&font=inter"},
+    {id: 2, email:"nadir@email.com",senha:"12345",nome:"Nadir da Silva",avatar : "https://placehold.co/100x100/d1d5db/111827.png?text=Mobile%20Preview&font=inter"},
+    {id: 3, email:"joel@email.com",senha:"12345",nome:"Joel Cândido",avatar : "https://placehold.co/100x100/d1d5db/111827.png?text=Mobile%20Preview&font=inter"},
+    {id: 4, email:"tonho@email.com",senha:"12345",nome:"Antonio dos Santos",avatar : "https://placehold.co/100x100/d1d5db/111827.png?text=Mobile%20Preview&font=inter"},
+    {id: 5, email:"jo@email.com",senha:"12345",nome:"Jaquim Jaleno",avatar : "https://placehold.co/100x100/d1d5db/111827.png?text=Mobile%20Preview&font=inter"},
 ];
-
-//O que é uma STRING?
-// let frase = "Estou com muito SONO!";
-// console.log(frase.length);
-
-// for(let x = 0; x < frase.length; x++){
-//     if(frase[x].toLowerCase() === "o")
-//         console.log(x,frase[x]);
-// }
-
-for (let index = 0; index < usuarios.length; index++) {
-        console.log(usuarios[index].id,usuarios[index].nome);
-}
-
- 
-
-
 
 
 //Compare os dado do form contra a lista de usuários
@@ -33,49 +16,75 @@ for (let index = 0; index < usuarios.length; index++) {
 const botao = document.querySelector("#btnEntrar");
 
 botao.addEventListener("click", (e)=>{
+
     e.preventDefault();
 
     //Capturando dados do campo de email e senha
-    const email = document.getElementById("idEmail");
-    const senha = document.getElementById("idSenha");
+    // const email = document.getElementById("idEmail");
+    // const senha = document.getElementById("idSenha");
     
     //Declarando um objeto:
     let usuario = {
-        email: email.value,
-        senha: senha.value
+        email: document.getElementById("idEmail").value,
+        senha: document.getElementById("idSenha").value
     }
 
     console.log( usuario.email );
     console.log( usuario.senha );
 
-
-    const userDoBanco = {
-        email  : "email@email.com",
-        senha  : "12345",
-        nome   : "José das Couves",
-        avatar : "https://placehold.co/100x100/d1d5db/111827.png?text=Mobile%20Preview&font=inter"
-    }
+    // const userDoBanco = {
+    //     email  : "email@email.com",
+    //     senha  : "12345",
+    //     nome   : "José das Couves",
+    //     avatar : "https://placehold.co/100x100/d1d5db/111827.png?text=Mobile%20Preview&font=inter"
+    // }
 
     //Validação de dados do usuário
 
     try {
         if(usuario !== null){
-            if( (usuario.email === userDoBanco.email) && (usuario.senha === userDoBanco.senha) ){
-                    alert("Login realizado com sucesso!");
 
-                    //Informando o usuário que ele será redirecionado!
-                    const divMsg = document.getElementById("msg");
+            for(let x = 0 ; x < usuarios.length ; x++){
+                
+                if( (usuario.email === usuarios[x].email) && (usuario.senha === usuarios[x].senha) ){
+                        alert("Login realizado com sucesso!");
+    
+                        //Informando o usuário que ele será redirecionado!
+                        const divMsg = document.getElementById("msg");
 
-                    //Adicionando a msg em um <p></p> através de innerHTML
-                    divMsg.innerHTML = "<p>Você será redirecionado em 5 segundo.....</p>"
+                        let contador = 5;
 
-                    setTimeout(function(){
-                        //Redirecionando o usuário para a página de inicial!
-                        window.location.href = "../index.html"; 
-                    },5000);
-            }else{
+                        //Adicionando a msg em um <p></p> através de innerHTML
+                        divMsg.innerHTML = `<p>Você será redirecionado em ${contador} segundo.....</p>`;
+    
+                        const intervalo = setInterval( ()=>{
+
+                            contador--;
+
+                            divMsg.innerHTML = `<p>Você será redirecionado em ${contador} segundo.....</p>`;
+
+                            if(contador === 0){
+                                window.location.href = "../index.html";
+                                clearInterval(intervalo);
+                            }
+
+                        },1000 );
+
+
+                        //  const timeOut = setTimeout(function(){
+                        //     //Redirecionando o usuário para a página de inicial!
+                        //     window.location.href = "../index.html"; 
+
+                        //     clearTimeout(timeOut);
+
+                        // },5000);
+
+                        return;
+                    }
+                }
+                
                 throw new Error("Email ou senha incorretos!");
-            }
+                
         }else{
             throw new Error("Usuário não encontrado!");
         }
