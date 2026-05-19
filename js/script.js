82e3660c883bd654c3f737dd9871789b7f9071ed
murilloslopes <murillosivierolopes2310@gmail.com>
2026-05-19 20:54:26 -0300

    feat: Corrigindo redirect e ultilização da lista na validacao de login

--- js/script.js
+++ js/script.js
@@ -1,63 +1,80 @@
 //Criando uma lista de objetos:
 const usuarios = [
-    {id: 1, email:"email@email.com",senha:"12345",nome:"José das Couves",avatar : "https://placehold.co/100x100/d1d5db/111827.png?text=Mobile%20Preview&font=inter"},
-    {id: 2, email:"nadir@email.com",senha:"12345",nome:"Nadir das Batatas",avatar : "https://placehold.co/100x100/d1d5db/111827.png?text=Mobile%20Preview&font=inter"},
-    {id: 3, email:"joel@email.com",senha:"12345",nome:"Joel da Cândida",avatar : "https://placehold.co/100x100/d1d5db/111827.png?text=Mobile%20Preview&font=inter"},
-    {id: 4, email:"tonho@email.com",senha:"12345",nome:"Antonio das Alfaces",avatar : "https://placehold.co/100x100/d1d5db/111827.png?text=Mobile%20Preview&font=inter"},
-    {id: 5, email:"jo@email.com",senha:"12345",nome:"Jaquim das Ja",avatar : "https://placehold.co/100x100/d1d5db/111827.png?text=Mobile%20Preview&font=inter"},
+    {id: 1, email:"email@email.com",senha:"12345",nome:"José Menino",avatar : "https://placehold.co/100x100/d1d5db/111827.png?text=Mobile%20Preview&font=inter"},
+    {id: 2, email:"nadir@email.com",senha:"12345",nome:"Nadir da Silva",avatar : "https://placehold.co/100x100/d1d5db/111827.png?text=Mobile%20Preview&font=inter"},
+    {id: 3, email:"joel@email.com",senha:"12345",nome:"Joel Cândida",avatar : "https://placehold.co/100x100/d1d5db/111827.png?text=Mobile%20Preview&font=inter"},
+    {id: 4, email:"tonho@email.com",senha:"12345",nome:"Antonio alves",avatar : "https://placehold.co/100x100/d1d5db/111827.png?text=Mobile%20Preview&font=inter"},
+    {id: 5, email:"jo@email.com",senha:"12345",nome:"Jaquim vitor",avatar : "https://placehold.co/100x100/d1d5db/111827.png?text=Mobile%20Preview&font=inter"},
 ];
 
+for (let index = 0; index < usuarios.length; index++) {
+        console.log(usuarios[index].id,usuarios[index].nome);
+}
 
+// const botao = document.getElementById("btnEntrar");
+const botao = document.querySelector("#btnEntrar");
 
-
-
-
-
-const botao = document.getElementById("btnEntrar");
-
-botao.addEventListener("click", function(e){
+botao.addEventListener("click", (e)=>{
     e.preventDefault();
-    //Capturando dados do campo de email e senha
-    const email = document.getElementById("idEmail");
-    const senha = document.getElementById("idSenha");
     
     //Declarando um objeto:
     let usuario = {
-        email: email.value,
-        senha: senha.value
+        email: document.getElementById("idEmail").value,
+        senha: document.getElementById("idSenha").value,
     }
 
     console.log( usuario.email );
     console.log( usuario.senha );
 
 
-    const userDoBanco = {
-        email  : "email@email.com",
-        senha  : "12345",
-        nome   : "José das Couves",
-        avatar : "https://placehold.co/100x100/d1d5db/111827.png?text=Mobile%20Preview&font=inter"
-    }
-
     //Validação de dados do usuário
 
     try {
         if(usuario !== null){
-            if( (usuario.email === userDoBanco.email) && (usuario.senha === userDoBanco.senha) ){
+             
+            for(let x = 0 ; x < usuarios.length; x++) {
+
+                console.log(usuarios[x].nome);
+                console.log(usuarios[x].email);
+
+                 if( (usuario.email === usuarios[x].email) && (usuario.senha === usuarios[x].senha) ){
                     alert("Login realizado com sucesso!");
 
                     //Informando o usuário que ele será redirecionado!
                     const divMsg = document.getElementById("msg");
 
-                    //Adicionando a msg em um <p></p> através de innerHTML
-                    divMsg.innerHTML = "<p>Você será redirecionado em 5 segundo.....</p>"
+                    let contador = 5;
 
-                    setTimeout(function(){
-                        //Redirecionando o usuário para a página de inicial!
-                        window.location.href = "../index.html"; 
-                    },5000);
-            }else{
-                throw new Error("Email ou senha incorretos!");
+                    //Adicionando a msg em um <p></p> através de innerHTML
+                    divMsg.innerHTML = `<p>Você será redirecionado em ${cntador} segundo.....</p>`;
+
+                    const intervalo = setInterval ( ()=>{
+
+                        contador--;
+
+                        divMsg.innerHTML = `<p>Você será redirecionado em ${cntador} segundo.....</p>`;
+
+                        if(contador == 0){
+                            window.location,href = "../index.html";
+                            clearInterval(intervalo);
+                        }
+
+                    },1000 )
+
+                    // const timeOut = setTimeout(function(){
+                    //     //Redirecionando o usuário para a página de inicial!
+                    //     window.location.href = "../index.html"; 
+
+                    //     clearTimeout(timeOut);
+
+                    // },5000);
+
+                    return;
+                 }    
             }
+
+            throw new Error("Email ou senha incorretos!");
+
         }else{
             throw new Error("Usuário não encontrado!");
         }
@@ -67,21 +84,5 @@ botao.addEventListener("click", function(e){
         alert(error);
     }
 
-
-
-    //Crie um novo objeto com as propriedades email e senha.
-    //Compare contra os dados do usuário.
-    //Crie uma validação para os campos de e-mail e senha.
-    //Se a validacão for bem sucedida, exibir mensagem de sucesso, caso contrário, exiba mensagem de erro.
-
-
-
+    
 }); 
-
-
-
-
-
-
-
-
