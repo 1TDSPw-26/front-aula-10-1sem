1ad58b99b7cf1b2fb10d6584522c23dd3cc227ac
rodrigobanharelli <guigo.banharelli@gmail.com>
2026-05-19 22:57:41 -0300

    feat: aula 010

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 //Criando uma lista de objetos:
 const usuarios = [
-    {id: 1, email:"email@email.com",senha:"12345",nome:"José das Couves",avatar : "https://placehold.co/100x100/d1d5db/111827.png?text=Mobile%20Preview&font=inter"},
-    {id: 2, email:"nadir@email.com",senha:"12345",nome:"Nadir das Batatas",avatar : "https://placehold.co/100x100/d1d5db/111827.png?text=Mobile%20Preview&font=inter"},
-    {id: 3, email:"joel@email.com",senha:"12345",nome:"Joel da Cândida",avatar : "https://placehold.co/100x100/d1d5db/111827.png?text=Mobile%20Preview&font=inter"},
-    {id: 4, email:"tonho@email.com",senha:"12345",nome:"Antonio das Alfaces",avatar : "https://placehold.co/100x100/d1d5db/111827.png?text=Mobile%20Preview&font=inter"},
-    {id: 5, email:"jo@email.com",senha:"12345",nome:"Jaquim das Jacas",avatar : "https://placehold.co/100x100/d1d5db/111827.png?text=Mobile%20Preview&font=inter"},
+    {id: 1, email:"email@email.com",senha:"12345",nome:"José Menino",avatar : "https://placehold.co/100x100/d1d5db/111827.png?text=Mobile%20Preview&font=inter"},
+    {id: 2, email:"nadir@email.com",senha:"12345",nome:"Nadir da Silva",avatar : "https://placehold.co/100x100/d1d5db/111827.png?text=Mobile%20Preview&font=inter"},
+    {id: 3, email:"joel@email.com",senha:"12345",nome:"Joel Cândido",avatar : "https://placehold.co/100x100/d1d5db/111827.png?text=Mobile%20Preview&font=inter"},
+    {id: 4, email:"tonho@email.com",senha:"12345",nome:"Antonio dos Santos",avatar : "https://placehold.co/100x100/d1d5db/111827.png?text=Mobile%20Preview&font=inter"},
+    {id: 5, email:"jo@email.com",senha:"12345",nome:"Jaquim Jaleno",avatar : "https://placehold.co/100x100/d1d5db/111827.png?text=Mobile%20Preview&font=inter"},
 ];
 
 
@@ -12,78 +12,91 @@ const usuarios = [
 //Utilize estruura de repetição
 //Imprima a lista de usuários.
 
+// const botao = document.getElementById("btnEntrar");
+const botao = document.querySelector("#btnEntrar");
 
+botao.addEventListener("click", (e)=>{
 
-const botao = document.getElementById("btnEntrar");
-
-botao.addEventListener("click", function(e){
     e.preventDefault();
+
     //Capturando dados do campo de email e senha
-    const email = document.getElementById("idEmail");
-    const senha = document.getElementById("idSenha");
+    // const email = document.getElementById("idEmail");
+    // const senha = document.getElementById("idSenha");
     
     //Declarando um objeto:
     let usuario = {
-        email: email.value,
-        senha: senha.value
+        email: document.getElementById("idEmail").value,
+        senha: document.getElementById("idSenha").value
     }
 
     console.log( usuario.email );
     console.log( usuario.senha );
 
-
-    const userDoBanco = {
-        email  : "email@email.com",
-        senha  : "12345",
-        nome   : "José das Couves",
-        avatar : "https://placehold.co/100x100/d1d5db/111827.png?text=Mobile%20Preview&font=inter"
-    }
+    // const userDoBanco = {
+    //     email  : "email@email.com",
+    //     senha  : "12345",
+    //     nome   : "José das Couves",
+    //     avatar : "https://placehold.co/100x100/d1d5db/111827.png?text=Mobile%20Preview&font=inter"
+    // }
 
     //Validação de dados do usuário
 
     try {
-        
         if(usuario !== null){
-            if( (usuario.email === userDoBanco.email) && (usuario.senha === userDoBanco.senha) ){
-                alert("Login efetuado com sucesso!");
 
-                //informando o usuario que ele será redirecionado para a página inicial em 5 segundos.
-                const divMsg = document.getElementById("msg");
+            for(let x = 0 ; x < usuarios.length ; x++){
+                console.log(usuarios[x].nome);
+                console.log(usuarios[x].email);
+                if( (usuario.email === userDoBanco.email) && (usuario.senha === userDoBanco.senha) ){
+                        alert("Login realizado com sucesso!");
+    
+                        //Informando o usuário que ele será redirecionado!
+                        const divMsg = document.getElementById("msg");
+    
+                        //Adicionando a msg em um <p></p> através de innerHTML
+                        divMsg.innerHTML = "<p>Você será redirecionado em 5 segundo.....</p>"
+    
+                        setTimeout(function(){
+                            //Redirecionando o usuário para a página de inicial!
+                            window.location.href = "../index.html"; 
+                        },5000);
 
-                //Adicionando a msg em um <p></p> através de innerHTML
-                divMsg.innerHTML = "Você será redirecionado para a página inicial em 5 segundos.</p>";
-
-                setTimeout(function(){
-                        //Redirecionando o usuário para a página de inicial!
-                        window.location.href = "../index.html";
-                    },5000);
-
-
-            }else{
-                throw new Error("E-mail ou senha inválidos!");
-            }
-        
+                        return;
+                    }
+                }
+                
+                throw new Error("Email ou senha incorretos!");
+                
         }else{
             throw new Error("Usuário não encontrado!");
         }
+        
     } catch (error) {
         console.error(error);
         alert(error);
     }
 
-    //Crie um novo objeto com as propriedades email e senha.
-    //Compare contra os dados do usuário.
-    //Crie uma validação para os campos de e-mail e senha.
-    //Se a validacão for bem sucedida, exibir mensagem de sucesso, caso contrário, exiba mensagem de erro.
-
-
-
+    
 }); 
 
+// function validar(){
+//     //Função Tradicional!
+//     console.log("NÃO EXECUTE!!");
+// }
+// function validar(...args){
+//     let resultado = args[0] + args[1] + args[2];
+//     return resultado;
+// }
+
+// console.log(validar(2,2,4));
+
+// const validar = (a,b) => a+b;
+// console.log(validar(2,2));
+
+// const validar = (a,b) =>{
+//     return a+b;
+// }
+// console.log(validar(2,2));
 
 
-
-
-
-
-
+    // Tente realizar a validação da lista contra o objeto que recupera dos dados do form.
